b543addc2b272ff4626662a82dbbdcb7e0dc6fc8
Zsolt <5047426+bodzaital@users.noreply.github.com>
2022-01-27 09:51:19 +0100

    Add source map, refactor

--- ts-src/formula1_v2.ts
+++ ts-src/formula1_v2.ts
@@ -39,71 +39,45 @@ class Nameplate {
 		this.context = context;
 	}
 
+	private build(name: string, classList: string[], innerText: string = "") {
+		let element = document.createElement(name);
+		classList.forEach((e) => element.classList.add(e));
+		element.innerText = innerText;
+		
+		return element;
+	}
+
+	private appendChildren(target: HTMLElement, children: HTMLElement[]) {
+		children.forEach((e) => target.appendChild(e));
+	}
+
 	public Create(target: HTMLElement) {
-		let container = document.createElement("div");
-		container.classList.add("f1", "nameplate-short");
-
-		let position = document.createElement("div");
-		position.classList.add("position", this.context.grid.state);
-		position.innerText = this.context.grid.position.toString();
-
-		let separator = document.createElement("div");
-		separator.classList.add("separator");
-
-		let teamColor = document.createElement("div");
-		teamColor.classList.add(`${this.context.color}-bg`);
-
-		separator.appendChild(teamColor);
-
-		let nameAndTeam = document.createElement("div");
-		nameAndTeam.classList.add("name-and-team");
-
-		let nameAndNumber = document.createElement("div");
-		nameAndNumber.classList.add("name-and-number");
-
-		let name = document.createElement("div");
-		name.classList.add("name");
+		let container = this.build("div", ["f1", "nameplate-short"]);
 		
-		let firstname = document.createElement("span");
-		firstname.classList.add("firstname");
-		firstname.innerText = this.context.firstname;
+		let position = this.build("div", ["position", this.context.grid.state], this.context.grid.position.toString());
+		let separator = this.build("div", ["separator"]);
+		let teamColor = this.build("div", [`${this.context.color}-bg`]);
+		this.appendChildren(separator, [teamColor]);
 		
-		let lastname = document.createElement("span");
-		lastname.classList.add("lastname");
-		lastname.innerText = this.context.lastname.toUpperCase();
-
-		name.appendChild(firstname);
-		name.appendChild(lastname);
-
-		let number = document.createElement("div");
-		number.classList.add("number");
-		number.classList.add(`${this.context.color}-text`);
-		number.innerText = this.context.number.toString();
-
-		nameAndNumber.appendChild(name);
-		nameAndNumber.appendChild(number);
-
-		let team = document.createElement("div");
-		team.classList.add("team");
-		team.innerText = this.context.team;
-
-		nameAndTeam.appendChild(nameAndNumber);
-		nameAndTeam.appendChild(team);
-
-		let flagContainer = document.createElement("div");
-		flagContainer.classList.add("flag-container");
-
-		let flag = document.createElement("div");
-		flag.classList.add("flag");
+		let nameAndTeam = this.build("div", ["name-and-team"]);
+		let nameAndNumber = this.build("div", ["name-and-number"]);
+		let name = this.build("div", ["name"]);
+		let firstname = this.build("span", ["firstname"], this.context.firstname);
+		let lastname = this.build("span", ["lastname"], this.context.lastname.toUpperCase());
+		this.appendChildren(name, [firstname, lastname]);
+		
+		let number = this.build("div", ["number", `${this.context.color}-text`], this.context.number.toString());
+		this.appendChildren(nameAndNumber, [name, number]);
+		
+		let team = this.build("div", ["team"], this.context.team);
+		this.appendChildren(nameAndTeam, [nameAndNumber, team]);
+		
+		let flagContainer = this.build("div", ["flag-container"]);
+		let flag = this.build("div", ["flag"]);
 		flag.style.backgroundImage = `linear-gradient(65deg, rgba(0, 0, 0, 1) 15%, rgba(0, 0, 0, 0) 40%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 1) 80%), url('https://flagcdn.com/${this.context.flag}.svg')`;
-
-		flagContainer.appendChild(flag);
-
-		container.appendChild(position);
-		container.appendChild(separator);
-		container.appendChild(nameAndTeam);
-		container.appendChild(flagContainer);
-
-		target.appendChild(container);
+		this.appendChildren(flagContainer, [flag]);
+		
+		this.appendChildren(container, [position, separator, nameAndTeam, flagContainer]);
+		this.appendChildren(target, [container]);
 	}
 }
